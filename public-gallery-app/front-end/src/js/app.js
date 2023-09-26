dc2f1e7fa9496785f69fb087412d90b44cd9e2ff
Dilan Asiri <dilanasiri95@gmail.com>
2023-09-27 00:01:15 +0530

    change a dependancies

--- public-gallery-app/front-end/src/js/app.js
+++ public-gallery-app/front-end/src/js/app.js
@@ -1,11 +1,18 @@
+import {saveAs} from 'file-saver';
+
 const overlay = $("#overlay");
 const btnUpload = $("#btn-upload");
 const dropZoneElm = $("#drop-zone");
 const mainElm = $("main");
 const REST_API_URL = `http://localhost:8080/gallery`;
+const cssLoaderHtml = '<div class="lds-facebook"><div></div><div></div><div></div></div>';
+const imgPreviewElm = $("#preview-image");
 const popupWindowElm = $(".popup-image");
+const btnClose = $("#btn-close");
 const btnNext = $("#btn-next");
 const btnPrevious = $("#btn-previous");
+let imgUrlList = [];
+let currentImgIndex = 0;
 
 loadAllImages();
 btnUpload.on('click', () => overlay.removeClass('d-none'));
@@ -23,6 +30,58 @@ $(document).on('keyup', (eventData)=>{
 dropZoneElm.on('dragover', (evt)=> {
     evt.preventDefault();
 });
+dropZoneElm.on('drop', (evt)=> {
+    evt.preventDefault();
+    const droppedFiles = evt.originalEvent.dataTransfer.files;
+    const imageFiles = Array.from(droppedFiles).filter(file => file.type.startsWith("image/"));
+    if (!imageFiles.length) return;
+    overlay.addClass('d-none');
+    uploadImages(imageFiles);
+});
+overlay.on('dragover', (evt) => evt.preventDefault());
+overlay.on('drop', (evt) => evt.preventDefault());
+mainElm.on('click', '.image .download', (eventData) => {
+    eventData.stopPropagation();
+
+    const imgDiv = $(eventData.target).closest('.image');
+    const imagePath = imgDiv.css('background-image').replace(/^url\(['"](.+)['"]\)/, '$1');
+    const fileName = getFileName(imagePath);
+    console.log(imagePath);
+
+    downloadImage(imagePath, fileName);
+});
+mainElm.on('click', ".image #download:not(.download)", (eventData)=>{
+    popupWindowElm.removeClass('d-none');
+    const imgDiv = $(eventData.target).closest('.image');
+    const imageUrl = imgDiv.css('background-image').replace(/^url\(['"](.+)['"]\)/, '$1');
+
+    const selectedImgIndex = imgUrlList.findIndex((value) => value === imageUrl);
+    showPopupImage(selectedImgIndex);
+});
+btnNext.on('click', ()=>{
+    const nextIndex = currentImgIndex + 1;
+    showPopupImage(nextIndex);
+});
+btnPrevious.on('click', ()=>{
+    const previousIndex = currentImgIndex - 1;
+    showPopupImage(previousIndex);
+});
+btnClose.on('click', (eventData)=>{
+    $(eventData.target).parent().addClass('d-none');
+});
+function showPopupImage(index) {
+    if (index >= 0 && index < imgUrlList.length) {
+        imgPreviewElm.css('background-image', `url('${imgUrlList[index]}')`);
+        currentImgIndex = index;
+    }
+}
+function getFileName(imagePath){
+    return imagePath.substring(imagePath.lastIndexOf("/") + 1);
+}
+function downloadImage(imagePath, fileName) {
+    // let fileSaver = require('file-saver');
+    saveAs(imagePath, fileName);
+}
 
 function loadAllImages(){
     const jqxhr = $.ajax(`${REST_API_URL}/images`);
@@ -45,4 +104,29 @@ function loadAllImages(){
         });
     });
     jqxhr.fail(()=>{});
+}
+function uploadImages(imageFiles){
+    const formData = new FormData();
+    imageFiles.forEach(imageFile => {
+        const divElm = $(`<div class="image loader"></div>`);
+        divElm.append(cssLoaderHtml);
+        mainElm.append(divElm);
+
+        formData.append("images", imageFile);
+    });
+    const jqxhr = $.ajax(`${REST_API_URL}/images`, {
+        method: 'POST',
+        data: formData,
+        contentType: false,
+        processData: false
+    });
+    jqxhr.done((imageUrlList)=>{
+        imageUrlList.forEach(imageUrl => {
+            const divElm = $(".image.loader").first();
+            divElm.css('background-image', `url('${imageUrl}')`);
+            divElm.empty();
+            divElm.removeClass('loader');
+        });
+    });
+    jqxhr.always(() => $(".image.loader").remove());
 }
